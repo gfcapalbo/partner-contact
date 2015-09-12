1502494a64896d4749a23b32f0f2f7905e0b8154
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-09-12 03:25:29 +0200

    [FIX] Some fixes to make partner_extra_address really work.

--- partner_extra_address/model/res_partner.py
+++ partner_extra_address/model/res_partner.py
@@ -28,19 +28,26 @@ class Partner(models.Model):
     def _compute_vals(self, vals):
         """Compute vals for create and write"""
         
-        def get_val(field, vals, record):
-            return field in vals and vals[field] or record[field]
+        def get_val(field, vals, record, m2o=False):
+            """Value from screen or from database."""
+            if field in vals:
+                return vals[field]  # Might be False for boolean
+            elif m2o:
+                return record[field].id
+            else:
+                return record[field]
 
         # Make sure we have only one default per type
         for record in self:
             if not get_val('active', vals, record):
                 vals['type_default'] = False  # inactive can not be default
             elif get_val('type_default', vals, record):
-                parent_id = get_val('parent_id', vals, record)
+                parent_id = get_val('parent_id', vals, record, m2o=True)
                 if parent_id:
                     type = get_val('type', vals, record)
                     # Find all records that might need to be reset
                     reset_partners = self.search([
+                        ('id', '!=', record.id),
                         ('parent_id', '=', parent_id),
                         ('type', '=', type),
                         ('type_default', '=', True),
@@ -59,5 +66,34 @@ class Partner(models.Model):
         """Call _compute_vals from write."""
         self._compute_vals(vals)
         return super(Partner, self).write(vals)
+
+    @api.multi
+    def address_get(self, adr_pref=None):
+        """Preprocess standard address_get to return default address for
+        the address types specified in adr_pref, that have a default for the
+        partners indicated. This has nothing to do with the global default
+        address, which is rather the fallback address if an address of the
+        desired type can not be found."""
+        parent_id = self.id
+        defaults_found = {}
+        if parent_id:  # Default makes no sense otherwise
+            adr_pref = set(adr_pref or [])
+            for type in adr_pref:
+                default_address = self.search([
+                    ('id', '=', parent_id),
+                    ('type', '=', type),
+                    ('type_default', '=', True),
+                ])
+                if default_address:
+                    adr_pref.pop(type)
+                    defaults_found[type] = default_address.id
+        result = super(Partner, self).address_get(adr_pref=adr_pref)
+        result.update(defaults_found)
+        return result
     
-    type_default = fields.Boolean(string='Default for address type')
+    # ==== Start field definitions
+    type_default = fields.Boolean(
+        string='Default for address type',
+        default=False,
+    )
+    # ====== End field definitions
